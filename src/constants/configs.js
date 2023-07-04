34641f7082e8fb444f072801690a8d4763f1578a
camewell <130561684+camewell071@users.noreply.github.com>
2023-07-04 17:42:28 +0700

    chore: update env

--- src/constants/configs.js
+++ src/constants/configs.js
@@ -1,11 +1,12 @@
+const isProduction = process.env.REACT_APP_ENV === 'production';
+
 const TC_LAYER2 = {
-    RPC: 'https://l2-node.regtest.trustless.computer/',
-    CHAIN: 42070,
-    Explorer: 'https://nos-explorer.regtest.trustless.computer/',
-    Name: "NOS (Test)"
+    RPC: isProduction ? 'https://node.l2.trustless.computer/' : 'https://l2-node.regtest.trustless.computer/',
+    CHAIN: isProduction ? 42213 : 42070,
+    Name: isProduction ? "NOS" : "NOS (Test)"
 };
 
-const CONTRACT_ADDRESS = '0x7Ec889fa161d5FE6102A8D8Caf6e25d7f0eeBD0d';
+const CONTRACT_ADDRESS = isProduction ? '0xE56fe77d7a69a9c633c2Bb71D6cE0807cC7452CA' : '0x7Ec889fa161d5FE6102A8D8Caf6e25d7f0eeBD0d';
 
 const SLEEP_TIME = 500;
 const COUNTER_TIME = 500;
